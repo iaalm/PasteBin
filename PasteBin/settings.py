157f585cc222a042f4ded271112b62084fcd801e
iaalm <iaalmsimon@gmail.com>
2014-10-07 12:58:09 +0800

    dj-database-url

--- PasteBin/settings.py
+++ PasteBin/settings.py
@@ -55,12 +55,9 @@ WSGI_APPLICATION = 'PasteBin.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 # Internationalization
